ec090ea1c78e3b8ea3c45ecd477452835711596a
daedalus_II <genericraxacoricofallapatorian@gmail.com>
2018-09-21 23:07:37 -0400

    alskjfsaldkjfl

--- HackCMU/CV.cc
+++ HackCMU/CV.cc
@@ -19,7 +19,7 @@ int main(){
         Mat frame;
         cap >> frame; // get a new frame from camera
         cvtColor(frame, edges, COLOR_BGR2GRAY);
-        GaussianBlur(edges, edges, Size(    7,7), 1.5, 1.5);
+        GaussianBlur(edges, edges, Size(7,7), 1.5, 1.5);
         Canny(edges, edges, 0, 30, 3);
         imshow("edges", edges);
         if(waitKey(30) >= 0) break;
